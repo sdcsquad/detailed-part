66067a0640c9afa9259e59db809c58762ac4ab1b
woobin huh <madgb00@gmail.com>
2018-10-24 16:45:15 -0700

    working on server01 and init webpack

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const router = require('./routes');
+const router = require('./routes')(app, Details);
 const db = require('../db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 8080;
-app.listen(port, ()=>{
+app.listen(port, () => {
 	console.log('working on ', port);
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,26 @@
-module.exports = (app) => {
+module.exports = (app, Details) => {
+	//get all details
 	app.get('/api/details', (req, res) => {
 		res.end();
 	})
 
+	//get one detail
 	app.get('api/details/:item-id', (req, res) => {
+		res.end();
+	})
+
+	//create a detail
+	app.post('/api/details/', (req, res) => {
+		res.end();
+	})
+
+	//update a detail
+	app.put('/api/details/:item-id', (req, res) => {
+		res.end();
+	})
+
+	//delete a detail
+	app.delete('/api/details/:item-id', (req, res) => {
 		res.end();
 	})
 }
