de9d0851ea3083f9cb2ac8c465a7ecfc4c990a1e
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-27 17:38:43 -0800

    changing cassandra contact points to EC2 cluster IP address

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const cassandra = require('cassandra-driver');
 
-const db = new cassandra.Client({ contactPoints: ['localhost'], keyspace: 'datahaus' });
+const db = new cassandra.Client({ contactPoints: ['172.31.3.36'], keyspace: 'datahaus' });
 
 module.exports = db;
